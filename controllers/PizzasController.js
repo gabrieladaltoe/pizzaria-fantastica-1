932bdbbac7883979996d23b219c5f670495d1782
gabrieladaltoe <gabrieladaltoe@gmail.com>
2021-07-28 22:04:11 -0300

    rota pizzas

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,5 +1,21 @@
+const pizzas = require('../database/Pizzas.json')
+
 module.exports = {
 	index: (req, res)=>{
-		res.render("index");
-	}
+		res.render("index", {pizzas : pizzas});
+	},
+	show: (req,res)=>{
+
+		const idPizza = req.params.id
+		//res.send(req.params.id)
+		//1. CARREGAR PIZZA DE ID PASSADO EM PIZZAS.FIND
+		//2. RENDERIZAR A VIEW PIZZA PASSANDO A PIZZA ENCONTRADA
+		//3. colocar ingredintes nome
+		res.render("pizza", {pizzas})
+	},
+
+	/*
+	search: (req,res)=>{
+		pizzas.find(x => x == pizzas.nome)
+	}*/
 }
